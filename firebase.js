97d56a520afc64bb85ec8c1a51f00925a7097a6a
Max Mandia <mmandia511@gmail.com>
2023-01-24 15:38:18 -0500

    new domain

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCzmM3ztf8x304erx53esZfelriUfgqCAo",
-  authDomain: "gpt-sms.firebaseapp.com",
+  authDomain: "https://gpt-sms-web.vercel.app/",
   projectId: "gpt-sms",
   storageBucket: "gpt-sms.appspot.com",
   messagingSenderId: "85766567466",
