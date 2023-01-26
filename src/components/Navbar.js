ca1e106710f516af37dbde1ead9f38711b20be74
Max Mandia <mmandia511@gmail.com>
2023-01-25 19:32:50 -0500

    feat: made navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Logo from "../assets/logo.svg";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import styles from "../styles/Navbar.module.css";
 function Navbar() {
   return (
-    <div>
+    <div className={styles.navContainer}>
       <Logo />
+      <HamburgerMenuIcon height={30} width={30} />
     </div>
   );
 }
